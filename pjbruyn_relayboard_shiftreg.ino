f97f7e9180c4982e1bf75a458b3ae84647f15ddd
zilog <dropmeaword@gmail.com>
2012-11-26 19:07:04 +0100

    Added support for potentiometer

--- pjbruyn_relayboard_shiftreg.ino
+++ pjbruyn_relayboard_shiftreg.ino
@@ -7,11 +7,15 @@
 #include "ShiftRegLib.h"
 
 // pin connected to ST_CP of 74HC595
-int srpin_latch = 8;
+int srpin_latch = 10;
 // pin connected to SH_CP of 74HC595
-int srpin_clock = 12;
+int srpin_clock = 9;
 // pin connected to DS of 74HC595
-int srpin_data = 11;
+int srpin_data = 12;
+
+// analog input pin for pontentiometer
+int potpin_ain = 3;
+int potval = 0;
 
 ShiftRegLib sr;
 
@@ -22,8 +26,15 @@ void setup() {
   sr.configure(srpin_latch, srpin_clock, srpin_data);
 }
 
+void pot_init() {
+  pinMode(potpin_ain, INPUT);
+}
+
+int pot_read() {
+  return analogRead(potpin_ain);
+}
+
 void loop() {
-  //count up routine
   for (int j = 0; j < 256; j++) {
     sr.latchDown();
       //count up on GREEN LEDs
@@ -32,7 +43,15 @@ void loop() {
       //sr.writeByte(255-j);
     sr.latchUp();
     
-    delay(1000);
-  }
-}
+    delay(100);
+    
+    //count up routine
+    int currentReading = pot_read();
+    if( currentReading != potval ) {
+      potval = currentReading;
+      Serial.println(potval);
+    } // if
+  } // for
+  
+} // loop
 
