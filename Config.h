7ace222be3b45fc634de1b6b9c7a56eba7d55084
zilog <dropmeaword@gmail.com>
2012-12-14 21:31:45 +0100

    Created a simple way to define the states using a table. Added a way to translate the potentiometer value to a timeout.

---
+++ Config.h
@@ -0,0 +1,32 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/* increase the value of TOTAL_STATES to match the number of lines
+ * in the outStates list
+ * remember that each state in the list is composed of 8 values
+ * when the value = 1 it means the relay is on (device plugged is on)
+ * when the value = 0 it means the relay is off (device plugged is off)
+ */
+#define TOTAL_STATES 10
+int outStates[TOTAL_STATES][8] = {
+  {1, 1, 1, 1, 1, 1, 1, 1},  // 255
+  {0, 1, 0, 1, 0, 1, 0, 1},  //  85
+  {1, 0, 1, 0, 1, 0, 1, 0},  // 170
+  {0, 0, 0, 0, 0, 0, 1, 1},  //   3
+  {0, 0, 0, 0, 0, 1, 1, 0},  //   6
+  {0, 0, 0, 0, 1, 1, 0, 0},  //  12
+  {0, 0, 0, 1, 1, 0, 0, 0},  //  24
+  {0, 0, 1, 1, 0, 0, 0, 0},  //  48
+  {0, 1, 1, 0, 0, 0, 0, 0},  //  96
+  {1, 1, 0, 0, 0, 0, 0, 0},  // 192
+};
+
+/* to define the maximum timeout keep the following in mind:
+ * multiply the number of seconds by 1000
+ * multiply the number of minutes by 60000 (or 60*1000)
+ * so a delay of 3 seconds would be 3*1000
+ * and a delay of 5 minutes would be 5*60*1000
+ */
+#define MAX_TIMEOUT 6*1000 // in milliseconds ==> (minutes*60*1000)
+
+#endif // __CONFIG_H__
--- pjbruyn_relayboard_shiftreg.ino
+++ pjbruyn_relayboard_shiftreg.ino
@@ -5,6 +5,7 @@
  * (cc) 2012 Luis Rodil-Fernandez
  */
 #include "ShiftRegLib.h"
+#include "Config.h"
 
 // pin connected to ST_CP of 74HC595
 int srpin_latch = 10;
@@ -19,11 +20,54 @@ int potval = 0;
 
 ShiftRegLib sr;
 
+unsigned long tmark = 0;  // reference timestamp
+unsigned long int interval = MAX_TIMEOUT;  // milliseconds to wait
+
+#define TIMECTL_MAXTICKS  4294967295L
+#define TIMECTL_INIT      0
+int timeout(unsigned long tinterval, unsigned long *treference){
+  unsigned long timeCurrent;
+  unsigned long timeElapsed;
+  int result = false;
+  
+  timeCurrent = millis();
+  if(timeCurrent < *treference) {  // rollover detected
+    timeElapsed = (TIMECTL_MAXTICKS-*treference)+timeCurrent;  //elapsed=all the ticks to overflow + all the ticks since overflow
+  }
+  else {
+    timeElapsed = timeCurrent - *treference;  
+  }
+
+  if(timeElapsed >= tinterval) {
+    *treference = timeCurrent;
+    result = true;
+  }
+  
+  return(result);  
+}
+
+int map(int value, int s1, int e1, int s2, int e2) {
+    float ftmp = ( s2 + (e2 - s2) * ((float)(value - s1) / (float)(e1 - s1)) );
+    return ceil(ftmp);
+}
+
+int arr2int(int *arr) {
+  int i;
+  int retval = 0;
+
+  for(i = 0; i < 8; i++) {
+    int val = arr[i];
+    val = val << (7 - i);
+    retval |= val;
+  }
+  return retval;
+}
 
 void setup() {
   // start Serial for debuging purposes	
   Serial.begin(9600);
   sr.configure(srpin_latch, srpin_clock, srpin_data);
+  pot_init();
 }
 
 void pot_init() {
@@ -35,23 +79,31 @@ int pot_read() {
 }
 
 void loop() {
-  for (int j = 0; j < 256; j++) {
+
+  for(int j = 0; j < TOTAL_STATES; j++) {
+    int *vec = outStates[j];
+    int r = arr2int(vec);
+
     sr.latchDown();
       //count up on GREEN LEDs
-      sr.writeByte(j); 
+      sr.writeByte(r); 
       //count down on RED LEDs
       //sr.writeByte(255-j);
     sr.latchUp();
-    
-    delay(100);
+
+    delay(interval);
     
     //count up routine
     int currentReading = pot_read();
     if( currentReading != potval ) {
       potval = currentReading;
-      Serial.println(potval);
+      interval = map(potval, 0, 1024, 0, MAX_TIMEOUT); 
+      Serial.print("interval = ");
+      Serial.print(interval);
+      Serial.print("ms.\n");
     } // if
-  } // for
-  
+  }
+
 } // loop
+
 
